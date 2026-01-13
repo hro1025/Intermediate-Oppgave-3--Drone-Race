729b4184eb204260b6ec5dd14116463794cead25
roan <hro102025@gmail.com>
2026-01-13 10:06:10 +0100

    init

---
+++ Classes/ControlTower.cs
@@ -0,0 +1,22 @@
+namespace Intermediate_Oppgave_3__Drone_Race.Classes;
+
+
+public class ControlTower
+{
+   
+
+    public void Fly()
+    {
+        Console.WriteLine("Start flying");
+
+        for (int i = 0; i < 15; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Finished flying");
+    }
+        
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Intermediate_Oppgave_3__Drone_Race.Classes;
+namespace Intermediate_Oppgave_3__Drone_Race;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var controlTower = new ControlTower;
+         var t1= new Thread(controlTower.Fly);
+         var t2 = new Thread(controlTower.Fly);
+         t1.Start();
+         t2.Start();
+
+
+
+        t1.Join(); 
+        t2.Join(); 
+    }
+}
